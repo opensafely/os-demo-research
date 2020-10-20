b1ba78e524f4d36b67704a905565142f2b68119e
wjchulme <25637345+wjchulme@users.noreply.github.com>
2020-10-20 11:56:49 +0100

    add plots, refactor

---
+++ rmarkdown/0 import.R
@@ -0,0 +1,21 @@
+
+library('tidyverse')
+library('jsonlite')
+
+
+md_list <- jsonlite::fromJSON(here::here("lib","measures_dict.json"), simplifyVector = FALSE, flatten=FALSE)
+md_tbl <- md_list %>%
+  enframe(name="measure", value="dictionary") %>%
+  unnest_wider(dictionary) %>% 
+  unnest_longer(groups, values_to="list", indices_to="group") %>% rename(var_label=label) %>% 
+  unnest_wider(list)  %>% 
+  unnest_wider(measure_args)
+
+measures <- md_tbl %>%
+  mutate(
+    data = map(id, ~read_csv(here::here("output",glue::glue("measure_{.}.csv")))),
+    plot_q = map()
+  )
+
+write_rds(measures, here::here("output", "collected_measures.rds"), compress="xz")
+
