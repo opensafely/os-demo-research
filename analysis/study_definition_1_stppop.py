555a76fe6342739a045eaeec38de08159eade669
wjchulme <25637345+wjchulme@users.noreply.github.com>
2020-11-10 14:06:15 +0000

    simplify stppop

--- analysis/study_definition_1_stppop.py
+++ analysis/study_definition_1_stppop.py
@@ -16,18 +16,15 @@ index_date = "2020-01-01"
 
 study = StudyDefinition(
 
-    default_expectations={
-        "date": {"earliest": index_date, "latest": "today"}, # date range for simulated dates
-        "rate": "uniform", # occurrance rate for simulated dates
-        "incidence": 1, # proportion where data is not missing, or where code is not present
-    },
+#    default_expectations={
+#        "date": {"earliest": index_date, "latest": "today"}, # date range for simulated dates
+#        "rate": "uniform", # occurrance rate for simulated dates
+#        "incidence": 1, # proportion where data is not missing, or where code is not present
+#    },
 
     # This line defines the study population
     population = patients.registered_as_of(
         index_date,
-        return_expectations={
-            "incidence": 0.95
-        }
     ),
 
     stp = patients.registered_practice_as_of(
